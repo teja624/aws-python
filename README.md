7f34b5dd129f711b2af7a885008a0408340004a9
Tom Hulbert <18743@student.bbc.qld.edu.au>
2015-12-08 22:38:39 +1000

    added blank space generator to aws.py added menu generator to aws.py changed start up sequence and timing

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 a python "browser" for AWS services
 
 --INSTALlATION--
-Open a new terminal session and use:
+Open a new terminal session and do:
 
 sudo pip3 install boto3
--- aws.py
+++ aws.py
@@ -4,15 +4,42 @@ import sys
 import time
 
 s3 = boto3.resource('s3')
-
+size = sys.stdout
 
 def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+#i'm feeling lazy...
+#Start lazy
 
+#blank space generator
+def blankSpace(lines):
+    blank = "\n"
+    if lines <= 1:
+        print("")
+    else:
+        print((lines - 1) * blank)
+
+#menu generator
+def menu(title,spacing,*args):
+    blank = "\n"
+    x = 0
+    if spacing < 1:
+        spacing = 1
+    print(title + ":")
+    for a in args:
+        x = x+1
+        print(a + "[" + str(x) + "]" + (spacing * blank))
+    option = input("Option: ")
+    return option
+#End lazy
+
+
+#intro "animation" and ASCII art
 def asciiArt():
     print("Welcome to...")
     time.sleep(1)
+    blankSpace(1)
     print("      ___           ___           ___                    ___           ___           ___           ___           ___           ___     ")
     print("     /\  \         /\__\         /\  \                  /\  \         |\__\         /\  \         /\__\         /\  \         /\__\    ")
     print("    /::\  \       /:/ _/_       /::\  \                /::\  \        |:|  |        \:\  \       /:/  /        /::\  \       /::|  |   ")
@@ -24,11 +51,22 @@ def asciiArt():
     print("      /:/  /     \:\/:/  /     \:\/:/  /                  \/__/     \/__/         \/__/            /:/  /     \:\/:/  /       |::/  /  ")
     print("     /:/  /       \::/  /       \::/  /                                                           /:/  /       \::/  /        /:/  /   ")
     print("     \/__/         \/__/         \/__/                                                            \/__/         \/__/         \/__/    ")
-    print("\nWritten by Tom Hulbert")
-#Thanks to active state user Barry Walker for assistance with window sizing
-sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=28,cols=150))
+    blankSpace(1)
+    print("Written by Tom Hulbert")
+
+#menu declerations
+mainMenu = ['AWS Functions','Setup','Exit']
+awsMenu = ['S3','Other','Exit']
+
+
+
+#Thanks to Active State user Barry Walker for assistance with window sizing
+size.write("\x1b[8;{rows};{cols}t".format(rows=28,cols=150))
 
 clearScreen()
 asciiArt()
-time.sleep(10)
+time.sleep(5)
 clearScreen()
+print("For first time users select 'Setup'")
+blankSpace(1)
+print(menu('Main Menu',1,*mainMenu))
